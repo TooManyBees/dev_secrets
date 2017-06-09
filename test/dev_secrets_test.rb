f715acfc9b30bd40e7e2d2dcb957f86fe0f10ae4
Jesse Kipp <jesse@toomanybees.com>
2017-06-09 14:41:38 -0400

    Add test to verify correct enc file is loaded
    Add an encrypted secrets file plus two additional ones:
    * secrets.yml.enc
    * secrets_dev.yml.enc
    * secrets_3rdparty.yml.enc
    
    Put the key to `secrets_dev.yml.enc` in `secrets.yml.key`. Test verifies
    that the correct secrets file is loaded.

--- test/dev_secrets_test.rb
+++ test/dev_secrets_test.rb
@@ -4,4 +4,8 @@ class DevSecrets::Test < ActiveSupport::TestCase
   test "truth" do
     assert_kind_of Module, DevSecrets
   end
+
+  test "loads config/secrets.yml.enc" do
+    assert_equal Rails.application.secrets.this_file_name, "secrets_dev.yml.enc"
+  end
 end
